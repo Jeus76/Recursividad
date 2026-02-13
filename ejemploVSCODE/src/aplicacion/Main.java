4d16e4849b2bfdaf8651a4a9bdc72d6e724fcb37
Jeus64 <jesushm06@gmail.com>
2026-02-13 10:03:38 +0100

    Commit message:

---
+++ ejemploVSCODE/src/aplicacion/Main.java
@@ -0,0 +1,8 @@
+package aplicacion;
+import dominio.Mates;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Proyecto Java básico listo.");
+        System.out.println("Factorial de 5: " + Mates.factorial(5));
+    }
+}
---
+++ ejemploVSCODE/src/dominio/Mates.java
@@ -0,0 +1,7 @@
+public class Mates {
+    public static int factorial(int n){
+        if (n==0)
+            return 1;
+        else return n*factorial(n-1);
+    }
+}
